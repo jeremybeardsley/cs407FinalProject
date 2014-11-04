9d772f66dee1d65e0f8c6539d7974dde595c43f7
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-04 16:25:25 -0500

    Added class-level javadocs for AnimalFactory.

--- src/cs407final/AnimalFactory.java
+++ src/cs407final/AnimalFactory.java
@@ -1,6 +1,15 @@
 package cs407final;
 
-
+/**
+ * Class Animal Factory which creates animals to populate the creature creation game.
+ * Animals are created based on a String parameter, throwing a InvalidArgumentException
+ * if the String does not match a known animal.
+ * @see InvalidArgumentException
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-4-2014
+ */
 public class AnimalFactory {
    
     protected static AnimalFactory animalFactory=null;
