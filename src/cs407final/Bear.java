05f30806605a3164035567368a3903e653c838ce
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-04 16:25:56 -0500

    Added class-level javadocs for Bear.

--- src/cs407final/Bear.java
+++ src/cs407final/Bear.java
@@ -1,6 +1,16 @@
 
 package cs407final;
 
+/**
+ * Class Bear which represents a concrete Animal in the creature creation game.
+ * This class extends the Animal abstract class, and has a MovementStrategy of the 
+ * subclass RandomMoveStrategy, as well as the ability to eat and move.
+ * @see RandomMoveStrategy
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-4-2014
+ */
 public class Bear extends Animal{
     private MovementStrategy moveStrat = new RandomMoveStrategy();
     @Override
