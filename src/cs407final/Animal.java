3c5fa4acf0129d16f9cca889747ad75f85ad07b7
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-04 16:24:55 -0500

    Added class-level javadocs for Animal.

--- src/cs407final/Animal.java
+++ src/cs407final/Animal.java
@@ -1,5 +1,14 @@
 package cs407final;
-//This is a comment. 
+/**
+ * Abstract class Animal which represents an animal in the creature creation game. Subclasses
+ * of Animal can move, eat, and make a sound. Subclasses also have a MovementStrategy 
+ * moveStrat which defines how this Animal moves in the move() method.
+ * @see MovementStrategy
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-4-2014
+ */
 public abstract class Animal {
 
     private MovementStrategy moveStrat;
