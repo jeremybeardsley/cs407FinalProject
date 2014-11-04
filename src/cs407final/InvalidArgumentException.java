d59e9e98ac5a9d21629a93c0d4fbd0dfe9694ac8
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-04 16:26:23 -0500

    Added class-level javadocs for InvalidArgumentException.

--- src/cs407final/InvalidArgumentException.java
+++ src/cs407final/InvalidArgumentException.java
@@ -1,7 +1,15 @@
 
 package cs407final;
 
-
+/**
+ * Exception which is thrown when an animal which does not exist is attempted to be 
+ * created by the AnimalFactory class.
+ * @see AnimalFactory
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-4-2014
+ */
 public class InvalidArgumentException extends Exception {
 
     public InvalidArgumentException(String type_required) {
