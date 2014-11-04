e766b1b12ddb5eb86afbc499e3cbe8268eca7fb0
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-04 16:27:31 -0500

    Added class-level javadocs for RandomMoveStrategy.

--- src/cs407final/RandomMoveStrategy.java
+++ src/cs407final/RandomMoveStrategy.java
@@ -1,6 +1,16 @@
 
 package cs407final;
 
+/**
+ * Class RandomMoveStrategy which represents an animal's random movement unrelated to 
+ * any other animal in the creature creation game. FlightMoveStrategy implements the 
+ * MovementStrategy interface.
+ * @see MovementStrategy
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-4-2014
+ */
 public class RandomMoveStrategy implements MovementStrategy{
 
     @Override
