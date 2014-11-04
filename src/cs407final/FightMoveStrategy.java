595ccf2140f6b5b2cd84eedf5be6dd4215cab36b
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-04 16:27:16 -0500

    Added class-level javadocs for FightMoveStrategy.

--- src/cs407final/FightMoveStrategy.java
+++ src/cs407final/FightMoveStrategy.java
@@ -1,7 +1,16 @@
 
 package cs407final;
 
-
+/**
+ * Class FightMoveStrategy which represents an animal's movement based on attacking
+ * another animal in the creature creation game. FightMoveStrategy implements the 
+ * MovementStrategy interface.
+ * @see MovementStrategy
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-4-2014
+ */
 public class FightMoveStrategy implements MovementStrategy{
 
     @Override
