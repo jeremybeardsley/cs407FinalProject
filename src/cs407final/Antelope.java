5e40ebc9badf201bbc25a6a359a367cb284a521c
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-04 16:25:44 -0500

    Added class-level javadocs for Antelope.

--- src/cs407final/Antelope.java
+++ src/cs407final/Antelope.java
@@ -1,5 +1,15 @@
 package cs407final;
 
+/**
+ * Class Antelope which represents a concrete Animal in the creature creation game.
+ * This class extends the Animal abstract class, and has a MovementStrategy of the 
+ * subclass FlightMoveStrategy, as well as the ability to eat and move.
+ * @see FlightMoveStrategy
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-4-2014
+ */
 public class Antelope extends Animal {
 
     private MovementStrategy moveStrat = new FlightMoveStrategy();
