4474e830166730b747edd8eb1eacca0a4f166806
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-04 16:26:38 -0500

    Added class-level javadocs for FlightMoveStrategy.

--- src/cs407final/FlightMoveStrategy.java
+++ src/cs407final/FlightMoveStrategy.java
@@ -1,6 +1,16 @@
 
 package cs407final;
 
+/**
+ * Class FlightMoveStrategy which represents an animal's movement based on running from 
+ * another animal in the creature creation game. FlightMoveStrategy implements the 
+ * MovementStrategy interface.
+ * @see MovementStrategy
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-4-2014
+ */
 public class FlightMoveStrategy implements MovementStrategy {
 
     @Override
