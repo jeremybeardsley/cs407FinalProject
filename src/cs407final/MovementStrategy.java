7661f1d98a4135acf64a78f9553af1ac9ec927df
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-04 16:26:58 -0500

    Added class-level javadocs for MovementStrategy.

--- src/cs407final/MovementStrategy.java
+++ src/cs407final/MovementStrategy.java
@@ -1,6 +1,13 @@
 package cs407final;
 
-
+/**
+ * Interface MovementStrategy which represents the style of an Animal's movement in
+ * the creature creation game.
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-4-2014
+ */
 public interface MovementStrategy {
     public void move(String movementType);
         
