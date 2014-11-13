80a324ec4c1ffde581f34fb0af3bc43ad2861a38
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-12 21:12:53 -0500

    Added class-level javadocs for Head.

--- src/cs407final/Head.java
+++ src/cs407final/Head.java
@@ -1,5 +1,15 @@
 package cs407final;
 import java.util.Iterator;
+
+/**
+ * Class Head which represents the head of an Animal in the creature creation game.
+ * This Animal component can be made up of leaf body parts and/or another AnimalComposite.
+ * @see AnimalComposite
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-12-2014
+ */
 public class Head extends AnimalComposite{
   @Override
   protected double getWeightBaseTotal() {
