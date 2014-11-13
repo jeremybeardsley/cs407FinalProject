4867a662e7e056c182cb7adad1a1f09e7066326c
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-12 21:11:20 -0500

    Added class-level javadocs for AnimalComponentDecorator.

--- src/cs407final/AnimalComponentDecorator.java
+++ src/cs407final/AnimalComponentDecorator.java
@@ -1,4 +1,14 @@
 package cs407final;
+
+/**
+ * Abstract class AnimalComponentDecorator which provides the framework for creating
+ * a decorator to change AnimalComponent properties of a specific Animal.
+ * @see AnimalComponent
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-12-2014
+ */
 public abstract class AnimalComponentDecorator extends AnimalComponent{
   protected final AnimalComponent decoratedComponent;
   
