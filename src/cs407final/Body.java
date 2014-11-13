aad960e3dd1050368fc2ce0762ad804f809ad897
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-12 21:11:53 -0500

    Added class-level javadocs for Body.

--- src/cs407final/Body.java
+++ src/cs407final/Body.java
@@ -1,4 +1,15 @@
 package cs407final;
+
+/**
+ * Class Body which represents the body of an Animal in the creature creation game.
+ * This Animal component can be made up of leaf body parts (such as Head or Legs)
+ * and/or another AnimalComposite.
+ * @see AnimalComposite
+ * 
+ * @author Jeremy Beardsley, Andrew McCoy, Matt LeClerc, Andrew Possardt
+ * @version 1.0
+ * @since 11-12-2014
+ */
 public class Body extends AnimalComposite{
   @Override
   protected double getWeightBaseTotal() {
