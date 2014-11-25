8e537cff1102cc02a4a167cf0146f3b58a40ba2c
apossardt <andrewpossardt@my.ccsu.edu>
2014-11-24 23:18:29 -0500

    Edited comments

--- src/cs407final/environment/GasVolcanicEruption.java
+++ src/cs407final/environment/GasVolcanicEruption.java
@@ -1,8 +1,5 @@
 package cs407final.environment;
 
-/**
- * Specific car information
- */
 public class GasVolcanicEruption extends Eruption{
 
   public GasVolcanicEruption(boolean fallout,boolean persistance) {
